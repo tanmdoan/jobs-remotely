3a7e492df4eaee08a72f5a25070cd661c79e7a43
tdoan87 <tdoan87@gmail.com>
2014-12-03 17:23:56 -0700

    tweaked the repo

--- lib/jobs/remotely/job.rb
+++ lib/jobs/remotely/job.rb
@@ -1,13 +1,16 @@
 module Jobs
   module Remotely
     class Job
-      attr_accessor :title, :description, :source_url, :posted_on
+      attr_accessor :title, :description, :source_url, :posted_on, :location
 
       def initialize(entry)
         @title       = entry.title
         @description = entry.summary
         @source_url  = entry.url
         @posted_on   = entry.published.to_date
+        if entry.location
+          @location = entry.location
+        end
       end
     end
   end
